e2b35c8e55923a68f7dd647fd9b4fb343cb12973
Lucian Augusto <lucian.augusto.92@gmail.com>
2020-04-01 21:28:06 -0300

    Add Vets List, update the Vets Controller (Closes # 16) and refactor Map Implementation to abstract and automatic Id generation (Closes #23).

--- pet-clinic-web/src/main/java/com/lucianaugusto/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/lucianaugusto/mypetclinic/controllers/VetController.java
@@ -1,13 +1,28 @@
 package com.lucianaugusto.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lucianaugusto.mypetclinic.services.VetService;
+
+@RequestMapping({"/vets"})
 @Controller
 public class VetController {
+//	Service
+	private final VetService vetService;
+	
+//	Constructor
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
 
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() { // 
+	@RequestMapping({"", "/", "/index", "/index.html"})
+	public String listVets(Model model) { // 
+		
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index"; // Returns the string that corresponds to the index.html file inside the vets folder inside the
 		// templates folder.
 	}
