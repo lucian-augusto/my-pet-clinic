ebb467d0f5f793fd59c42ae82368b7cf41987d37
Lucian Augusto <lucian.augusto.92@gmail.com>
2020-04-06 23:33:50 -0300

    Add PetTypeService Map Service Implementation (Closes #29). Add PetTypes  to the bootstrp DataLoader class (Closes #32).

---
+++ pet-clinic-data/src/main/java/com/lucianaugusto/mypetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.lucianaugusto.mypetclinic.services;
+
+import com.lucianaugusto.mypetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
--- pet-clinic-web/src/main/java/com/lucianaugusto/mypetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/lucianaugusto/mypetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.lucianaugusto.mypetclinic.model.Owner;
+import com.lucianaugusto.mypetclinic.model.PetType;
 import com.lucianaugusto.mypetclinic.model.Vet;
 import com.lucianaugusto.mypetclinic.services.OwnerService;
+import com.lucianaugusto.mypetclinic.services.PetTypeService;
 import com.lucianaugusto.mypetclinic.services.VetService;
 import com.lucianaugusto.mypetclinic.services.map.OwnerServiceMap;
 import com.lucianaugusto.mypetclinic.services.map.VetServiceMap;
@@ -15,17 +17,20 @@ public class DataLoader implements CommandLineRunner {
 	
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) { // Takes any implementations of both interfaces and
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) { // Takes any implementations of both interfaces and
 		// bring them into the context by using Spring to inject the dependencies. Constructor-based Dependency Injection,
 		// doesn't require the @Autowired annotation.
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+//		Owner Data Loader
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
@@ -40,6 +45,7 @@ public class DataLoader implements CommandLineRunner {
 		
 		System.out.println("Loaded Owner...");
 		
+//		Vets Data loader
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Sam");
 		vet1.setLastName("Axe");
@@ -53,6 +59,17 @@ public class DataLoader implements CommandLineRunner {
 		vetService.save(vet2);
 		
 		System.out.println("Loaded Vets...");
+		
+//		PetType data loader
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType saveDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		PetType saveCaPetType = petTypeService.save(cat);
+		
+		System.out.println("Loaded PetTypes...");
 		
 	}
 	
