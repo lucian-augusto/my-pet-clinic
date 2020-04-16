bc46a06a579e57a02197c79fd30217c4b70fe1cc
Lucian Augusto <lucian.augusto.92@gmail.com>
2020-04-15 22:54:56 -0300

    Add Spring Data JPA for Vets (Closes #41) and fix the OwnerSDJpaService to use OwnerRepository correctly.

--- pet-clinic-data/src/main/java/com/lucianaugusto/mypetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/lucianaugusto/mypetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -1,28 +1,27 @@
 package com.lucianaugusto.mypetclinic.services.springdatajpa;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import com.lucianaugusto.mypetclinic.model.Owner;
+import com.lucianaugusto.mypetclinic.repositories.OwnerRepository;
 import com.lucianaugusto.mypetclinic.repositories.PetRepository;
 import com.lucianaugusto.mypetclinic.repositories.PetTypeRepository;
 import com.lucianaugusto.mypetclinic.services.OwnerService;
-import com.lucianaugusto.mypetclinic.services.map.OwnerServiceMap;
 
 @Service
 @Profile("springdatajpa")
 public class OwnerSDJpaService implements OwnerService {
 	
-	private final OwnerServiceMap ownerRepository;
+	private final OwnerRepository ownerRepository;
 	private final PetRepository petRepository;
 	private final PetTypeRepository petTypeRepository;
 	
 	
-	public OwnerSDJpaService(OwnerServiceMap ownerRepository, PetRepository petRepository,
+	public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
 			PetTypeRepository petTypeRepository) {
 		this.ownerRepository = ownerRepository;
 		this.petRepository = petRepository;
@@ -39,8 +38,7 @@ public class OwnerSDJpaService implements OwnerService {
 
 	@Override
 	public Owner findById(Long id) {
-		Optional<Owner> optionalOwner = ownerRepository.findById(id);
-		return optionalOwner.orElse(null);
+		return ownerRepository.findById(id).orElse(null);
 	}
 
 	@Override
