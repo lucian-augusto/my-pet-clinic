20bfb97c227184e8fde68bbe37d1e3ba6f739f4d
lucian.augusto <lucian@juno.com.br>
2020-11-18 22:55:16 -0300

    #65 - Refactor VetController and create a new endpoint to return the vets in a JSON format. Closes #65

--- pet-clinic-web/src/main/java/com/lucianaugusto/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/lucianaugusto/mypetclinic/controllers/VetController.java
@@ -1,29 +1,39 @@
 package com.lucianaugusto.mypetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.lucianaugusto.mypetclinic.model.Vet;
 import com.lucianaugusto.mypetclinic.services.VetService;
 
-@RequestMapping({"/vets"})
 @Controller
 public class VetController {
 //	Service
 	private final VetService vetService;
-	
+
 //	Constructor
 	public VetController(VetService vetService) {
 		super();
 		this.vetService = vetService;
 	}
 
-	@RequestMapping({"", "/", "/index", "/index.html", ".html"})
-	public String listVets(Model model) { // 
-		
+	@RequestMapping({ "/vets", "/vets/", "/vets/index", "/vets/index.html", "/vets.html" })
+	public String listVets(Model model) { //
+
 		model.addAttribute("vets", vetService.findAll());
-		
-		return "vets/index"; // Returns the string that corresponds to the index.html file inside the vets folder inside the
+
+		return "vets/index"; // Returns the string that corresponds to the index.html file inside the vets
+								// folder inside the
 		// templates folder.
+	}
+
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetJson() {
+		return vetService.findAll();
 	}
 }
